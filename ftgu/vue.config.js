8763a616fe335e361aaf7cfdb54c156d22671854
papermashea <molloy.shea@gmail.com>
2022-04-25 14:28:44 -0400

    updating vue config

--- ftgu/vue.config.js
+++ ftgu/vue.config.js
@@ -1,53 +1,51 @@
-//// bootstrap local image paths
-// const options = {
-//   transformAssetUrls: {
-//     video: ['src', 'poster'],
-//     source: 'src',
-//     img: 'src',
-//     image: 'xlink:href',
-//     'b-avatar': 'src',
-//     'b-img': 'src',
-//     'b-img-lazy': ['src', 'blank-src'],
-//     'b-card': 'img-src',
-//     'b-card-img': 'src',
-//     'b-card-img-lazy': ['src', 'blank-src'],
-//     'b-carousel-slide': 'img-src',
-//     'b-embed': 'src'
-//   }
-// }
+module.exports = {
+  module: {
+    rules: [
+      // ... other rules omitted
 
+      // this will apply to both plain `.scss` files
+      // AND `<style lang="scss">` blocks in `.vue` files
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+
+  configureWebpack: config => {
+    // Copy data directory into build
+    // copy-webpack-plugin is a dependency of Vue Cli
+    const CopyWebpackPlugin = require('copy-webpack-plugin')
+    config.plugins.push(
+      new CopyWebpackPlugin([
+        {
+          from: resolve('data'),
+          ignore: ['.*'],
+          to: 'data'
+        }
+      ])
+    )
+  }
 
 //// vue relative path imports
-// module.exports = {
-//   chainWebpack: config => {
-//     config.module
-//       .rule('images')
-//         .set('parser', {
-//           dataUrlCondition: {
-//             maxSize: 4 * 1024 // 4KiB
-//           }
-//         })
-//   }
-// }
+  chainWebpack: config => {
+    config.module
+      .rule('images')
+        .set('parser', {
+          dataUrlCondition: {
+            maxSize: 4 * 1024 // 4KiB
+          }
+        })
+  }
+
+}
+
 
 // const path = require('path')
 // function resolve(dir) {
 //   return path.join(__dirname, dir)
-// }
-
-// module.exports = {
-//   configureWebpack: config => {
-//     // Copy data directory into build
-//     // copy-webpack-plugin is a dependency of Vue Cli
-//     const CopyWebpackPlugin = require('copy-webpack-plugin')
-//     config.plugins.push(
-//       new CopyWebpackPlugin([
-//         {
-//           from: resolve('data'),
-//           ignore: ['.*'],
-//           to: 'data'
-//         }
-//       ])
-//     )
-//   }
 // }
